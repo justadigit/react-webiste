9741f8a525e601c7861fccd0e08a1d1f33e11fbe
internationalcybercity@gmail.comgitgit <letmecode123456@gmail.com>
2021-12-03 12:14:31 +0630

    Feature

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,47 +1,48 @@
-import React from 'react'
-import styled from 'styled-components'
-import Woman from "../img/woman.png"
+import React from 'react';
+import styled from 'styled-components';
+import Woman from '../img/woman.png';
+import AnimatedShape from './AnimatedShape';
 const Container = styled.div`
-  height:calc(100vh - 105px);
-  display:flex;
-  padding:10px;
-  justify-content:center;
-`
+  height: calc(100vh - 105px);
+  display: flex;
+  padding: 10px;
+  justify-content: center;
+`;
 const Left = styled.div`
-width:60%;
-display:flex;
-flex-direction: column;
-align-items:center;
-justify-content:center;
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 const Title = styled.h1`
-  font-size:60px;
-  width:60%;
-`
+  font-size: 60px;
+  width: 60%;
+`;
 const Desc = styled.div`
-font-size:20px;
-width:60%;
-`
+  font-size: 20px;
+  width: 60%;
+`;
 const Info = styled.div`
-width: 60%;
+  width: 60%;
   margin-top: 50px;
-display:flex;
-  align-items:center;
-  justify-content:space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 const Button = styled.button`
-padding:15px;
-background-color:darkblue;
-color:white;
-border-radius:10px;
-border:none;
-letter-spacing:2px;
-font-weight: bold;
-cursor: pointer;
+  padding: 15px;
+  background-color: darkblue;
+  color: white;
+  border-radius: 10px;
+  border: none;
+  letter-spacing: 2px;
+  font-weight: bold;
+  cursor: pointer;
 `;
 const Contact = styled.div`
-display:flex;
-flex-direction:column;
+  display: flex;
+  flex-direction: column;
 `;
 const Phone = styled.span`
   color: #f0667d;
@@ -53,33 +54,40 @@ const ContactText = styled.span`
   margin-top: 5px;
 `;
 const Right = styled.div`
-width:40%;
-display:flex;
-align-items:center; 
-justify-content:center;
+  width: 40%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 const Image = styled.img`
-width:100%;
+  width: 100%;
 
-object-fit:cover;
-`
+  object-fit: cover;
+`;
 const Intro = () => {
   return (
     <Container>
       <Left>
         <Title>Advantures in Creative Age</Title>
-        <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, neque corporis? Laudantium delectus voluptatibus architecto obcaecati fugit eaque incidunt magni, </Desc>
+        <Desc>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, neque
+          corporis? Laudantium delectus voluptatibus architecto obcaecati fugit
+          eaque incidunt magni,{' '}
+        </Desc>
         <Info>
           <Button>START A PRODUCT</Button>
           <Contact>
-          <Phone>Call Us (095)123 23 23</Phone>
-          <ContactText>Far any question or concern</ContactText>
+            <Phone>Call Us (095)123 23 23</Phone>
+            <ContactText>Far any question or concern</ContactText>
           </Contact>
         </Info>
       </Left>
-      <Right><Image src={Woman} /></Right>
+      <Right>
+        <Image src={Woman} />
+      </Right>
+      <AnimatedShape />
     </Container>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
